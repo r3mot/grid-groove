6e070deb2d6ecb4072376ef444746a5c2f3fa6fa
Corey <88360543+r3mot@users.noreply.github.com>
2024-12-17 19:07:12 -0800

    fix: metronome not in sync with global transport (#3)

--- src/features/transport/Metronome.tsx
+++ src/features/transport/Metronome.tsx
@@ -1,7 +1,8 @@
 import { MetronomeIcon } from '@/components/icons/Metronome'
 import { toggleVariants } from '@/components/ui/toggle'
 import { cn } from '@/lib/utils'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useSequenceStore } from '@/providers/store/sequenceStore'
+import { useEffect, useRef, useState } from 'react'
 import { getTransport, Loop, Synth, SynthOptions } from 'tone'
 import { RecursivePartial } from 'tone/build/esm/core/util/Interface'
 
@@ -23,31 +24,41 @@ export function Metronome() {
   const loop = useRef<Loop | null>(null)
   const [isPlaying, setIsPlaying] = useState(false)
 
+  const playbackBPM = useSequenceStore(state => state.playbackBPM)
+
   useEffect(() => {
     synth.current = new Synth(synthOptions).toDestination()
 
     loop.current = new Loop(time => {
       const transport = getTransport()
-      transport.setLoopPoints(0, '1m')
-      const step = Number(transport.position.toString().split(':')[1])
-      const note = step === 0 ? 'C6' : 'C5'
+      const elapsedSeconds = transport.seconds
+      const beatDuration = 60 / playbackBPM
+      const beatPosition = Math.floor(elapsedSeconds / beatDuration) % 4
+
+      const note = beatPosition === 0 ? 'C6' : 'C5'
       synth.current?.triggerAttackRelease(note, '16n', time)
-    })
+    }, '4n')
 
     return () => {
       synth.current?.dispose()
       loop.current?.dispose()
     }
-  }, [])
+  }, [playbackBPM])
 
-  const toggle = useCallback(() => {
+  const toggleMetronome = () => {
     if (isPlaying) {
       loop.current?.stop()
     } else {
-      loop.current?.start()
+      loop.current?.start(0)
     }
     setIsPlaying(prev => !prev)
-  }, [isPlaying])
+  }
+
+  useEffect(() => {
+    if (isPlaying && loop.current) {
+      loop.current.interval = `${60 / playbackBPM}s` // Adjust interval on BPM change
+    }
+  }, [playbackBPM, isPlaying])
 
   return (
     <button
@@ -55,7 +66,7 @@ export function Metronome() {
         toggleVariants({ variant: 'default' }),
         isPlaying && 'bg-blue-600 hover:bg-blue-700',
       )}
-      onClick={toggle}
+      onClick={toggleMetronome}
     >
       <MetronomeIcon />
       <span className='sr-only'>Toggle Metronome</span>
