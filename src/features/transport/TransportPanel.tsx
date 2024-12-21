a299ec80f52bc82277a63171bfe586ab2293c3ea
r3mot <cmendrella1@toromail.csudh.edu>
2024-12-21 13:24:53 -0800

    fix(TransportPanel): improve layout and responsiveness of header elements

--- src/features/transport/TransportPanel.tsx
+++ src/features/transport/TransportPanel.tsx
@@ -9,21 +9,23 @@ import { ClearStepsDialog } from './ClearStepsDialog'
 
 export function TransportPanel() {
   return (
-    <header className='flex items-center border rounded-md justify-centerw-full h-fit bg-card'>
-      <div className='flex items-center justify-between w-full gap-2 py-4 mx-auto max-w-7xl'>
-        <div className='flex items-center gap-2'>
+    <header className='sticky top-0 z-10 w-full border-b shadow-lg lg:relative lg:shadow-none lg:border lg:rounded-md bg-card'>
+      <div className='flex items-center justify-center w-full gap-4 py-2 mx-auto lg:py-4 lg:gap-2 lg:justify-between max-w-7xl'>
+        <div className='flex items-center gap-4 lg:gap-2'>
           <TransportControls />
           <Metronome />
           <ClearStepsDialog />
         </div>
-        <div className='flex items-center gap-2'>
+        <div className='items-center hidden gap-4 lg:flex lg:gap-2'>
           <StepCount />
           <DisplayModeTabs />
           <TransportBPM />
         </div>
-        <InsetBorder className='h-10'>
-          <SpectrumAnalyser height={36} />
-        </InsetBorder>
+        <div className='hidden lg:block'>
+          <InsetBorder className='h-10'>
+            <SpectrumAnalyser height={36} />
+          </InsetBorder>
+        </div>
       </div>
     </header>
   )
