d9b8625ef600d364da50d608188c28b4702c6566
r3mot <cmendrella1@toromail.csudh.edu>
2025-01-01 00:18:51 -0800

    fix(TransportPanel): add padding for improved layout on smaller screens

--- src/features/transport/TransportPanel.tsx
+++ src/features/transport/TransportPanel.tsx
@@ -10,7 +10,7 @@ import { ClearStepsDialog } from './ClearStepsDialog'
 export function TransportPanel() {
   return (
     <header className='sticky top-0 z-10 w-full border-b shadow-lg lg:relative lg:shadow-none lg:border lg:rounded-md bg-card'>
-      <div className='flex items-center justify-center w-full gap-4 py-2 mx-auto lg:py-4 lg:gap-2 lg:justify-between max-w-7xl'>
+      <div className='flex items-center justify-center w-full gap-4 py-2 mx-auto lg:py-4 lg:gap-2 lg:justify-between max-w-7xl px-4'>
         <div className='flex items-center gap-4 lg:gap-2'>
           <TransportControls />
           <Metronome />
