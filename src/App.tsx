c99cc5caab3228805ded038d4da8b70c72763efd
r3mot <cmendrella1@toromail.csudh.edu>
2025-01-01 00:19:12 -0800

    fix(App): enhance layout for better responsiveness on larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import { Keyboard } from '@/features/keyboard/Keyboard'
 
 export default function App() {
   return (
-    <main className='flex items-center justify-center h-screen bg-background '>
-      <div className='container mx-auto overflow-hidden '>
+    <main className='lg:flex items-center justify-center min-h-screen bg-background relative'>
+      <div className='lg:container mx-auto overflow-hidden'>
         <div className='space-y-1 overflow-hidden '>
           <div className='w-full space-y-1'>
             <TransportPanel />
             <StepSequencer />
           </div>
-          <div className='flex flex-col w-full gap-1 lg:flex-row'>
+          <div className='hidden xl:flex w-full gap-1'>
             <ChannelMixer />
             <Keyboard />
           </div>
