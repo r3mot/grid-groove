e294559d2b6b45b2504226274b89236f45cff13e
Corey <88360543+r3mot@users.noreply.github.com>
2025-11-11 13:00:05 -0800

    chore: upgrade tailwind v4 (#19)
    * fix: remove tailwind-animate to allow auto migration
    
    * run tailwind upgrade for migration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
